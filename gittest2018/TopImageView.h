4b2da28a23be31daeacb3c842fea218702139809
qicaitiankong <zihaoshuhan@163.com>
2019-02-28 15:20:41 +0800

    添加了新文件topimageView

---
+++ gittest2018/TopImageView.h
@@ -0,0 +1,17 @@
+//
+//  TopImageView.h
+//  gittest2018
+//
+//  Created by 李自豪 on 2019/2/28.
+//  Copyright © 2019年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TopImageView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ gittest2018/TopImageView.m
@@ -0,0 +1,21 @@
+//
+//  TopImageView.m
+//  gittest2018
+//
+//  Created by 李自豪 on 2019/2/28.
+//  Copyright © 2019年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import "TopImageView.h"
+
+@implementation TopImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
